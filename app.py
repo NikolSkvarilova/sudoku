4a2fd77cf6d99472cb617a487a2bd8acf66e5192
NikolSkvarilova <skvarilovanikol@gmail.com>
2020-05-01 14:55:57 +0000

    Edited white spaces

--- app.py
+++ app.py
@@ -6,13 +6,13 @@ import random
 import leveler
 
 class App:
-    def __init__(self):
-        self.sudokuFile = App.readJSON()
+  def __init__(self):
+    self.sudokuFile = App.readJSON()
 
-    @staticmethod
-    def readJSON():
-        with open('data.json') as JSONdata:
-            return json.load(JSONdata)
+  @staticmethod
+  def readJSON():
+    with open('data.json') as JSONdata:
+      return json.load(JSONdata)
 
 if __name__ == "__main__":
-    app = App()
+  app = App()
--- creator.py
+++ creator.py
@@ -12,120 +12,120 @@ import copy
 
 
 class Creator(Solver):
-    # Param: self.size = size of the sudoku (usually 9x9, so size = 9)
-    # Param: self.base = size of the little square (usually 3x3)creating seed puzzles
-    def __init__(self, size=9):
-        self.size = size
-        self.base = int(self.size ** (1 / 2))
-        self.emptySpace = 0
-        self.leveler = Leveler()
+  # Param: self.size = size of the sudoku (usually 9x9, so size = 9)
+  # Param: self.base = size of the little square (usually 3x3)creating seed puzzles
+  def __init__(self, size=9):
+    self.size = size
+    self.base = int(self.size ** (1 / 2))
+    self.emptySpace = 0
+    self.leveler = Leveler()
     
-    def generateBlankArr(self):
-        self.board = [[self.emptySpace for i in range(self.size)] for j in range(self.size)]
+  def generateBlankArr(self):
+    self.board = [[self.emptySpace for i in range(self.size)] for j in range(self.size)]
 
-    # Shifts elements of an array (parram: arr) by specific value (param: n)
-    def shift(self, arr, n):
-        temp = arr.copy()
-        for i in range(len(arr)):
-            arr[i] = temp[i - n]
+  # Shifts elements of an array (parram: arr) by specific value (param: n)
+  def shift(self, arr, n):
+    temp = arr.copy()
+    for i in range(len(arr)):
+      arr[i] = temp[i - n]
 
-        return arr
+    return arr
 
-    # Checks if all numbers in the sudoku are valid
-    # Checks if the sudoku is solved correctly
-    # We could do this with checking the pattern, but some sudokus can have more solutions
-    def checkIfValid(self):
-        for i in range(self.size):
-            for j in range(self.size):
-                if not self.checkValid(self.board[i][j], (i, j)):
-                    return False
+  # Checks if all numbers in the sudoku are valid
+  # Checks if the sudoku is solved correctly
+  # We could do this with checking the pattern, but some sudokus can have more solutions
+  def checkIfValid(self):
+    for i in range(self.size):
+      for j in range(self.size):
+        if not self.checkValid(self.board[i][j], (i, j)):
+          return False
                 
-        return True
+    return True
                     
-    # Generates 2D array with numbers
-    # (1) It gives the first row random numbers 1-9
-    # (2) Shifts the second and third row's elements by 3
-    # (3) The next one it shifts by 1 and repeat the (2) 
-    def generate(self):
-        self.generateBlankArr()
+  # Generates 2D array with numbers
+  # (1) It gives the first row random numbers 1-9
+  # (2) Shifts the second and third row's elements by 3
+  # (3) The next one it shifts by 1 and repeat the (2) 
+  def generate(self):
+    self.generateBlankArr()
 
-        # Generate the first row
-        nums = list(range(self.size))
+    # Generate the first row
+    nums = list(range(self.size))
 
-        for i in range(self.size):
-            self.board[0][i] = nums.pop(choice(range(len(nums))))
+    for i in range(self.size):
+      self.board[0][i] = nums.pop(choice(range(len(nums))))
 
-        # Shifting (1, 3, 3)
-        for i in range(1, self.size):
-            self.board[i] = self.board[i - 1].copy()
-            # If we are on the first row in the square
-            if i % self.base == 0:
-                self.board[i] = self.shift(self.board[i], 1)
-            # If we are somewhere else
-            else:
-                self.board[i] = self.shift(self.board[i], 3)
+    # Shifting (1, 3, 3)
+    for i in range(1, self.size):
+      self.board[i] = self.board[i - 1].copy()
+      # If we are on the first row in the square
+      if i % self.base == 0:
+        self.board[i] = self.shift(self.board[i], 1)
+      # If we are somewhere else
+      else:
+        self.board[i] = self.shift(self.board[i], 3)
 
-        # Checks if the sudoku is valid
-        if self.checkIfValid():
-            return True
+      # Checks if the sudoku is valid
+      if self.checkIfValid():
+        return True
 
-        else:
-            self.generate()
+      else:
+        self.generate()
 
-    # With change 50% to 50% for each number removes numbers in the board 
-    def removeCharacters(self):
-        for i in range(self.size):
-            for j in range(self.size):
-                if choice(range(6)) == 0:
-                    self.board[i][j] = self.emptySpace
+  # With change 50% to 50% for each number removes numbers in the board 
+  def removeCharacters(self):
+    for i in range(self.size):
+      for j in range(self.size):
+        if choice(range(6)) == 0:
+          self.board[i][j] = self.emptySpace
 
-    @staticmethod
-    def setupJSONfile(numberOfLevels=1):
-        data = {}
-        for i in range(1, numberOfLevels + 1):
-            data["level" + str(i)] = []
+  @staticmethod
+  def setupJSONfile(numberOfLevels=1):
+    data = {}
+    for i in range(1, numberOfLevels + 1):
+      data["level" + str(i)] = []
 
-        Creator.writeToJSONFile(data=data)
-        return data
+    Creator.writeToJSONFile(data=data)
+    return data
 
-    @staticmethod
-    def writeToJSONFile(path="./", fileName="data", data={}):
-        filePathNameWExt = './' + path + '/' + fileName + '.json'
-        with open(filePathNameWExt, 'w') as f:
-            json.dump(data, f)
+  @staticmethod
+  def writeToJSONFile(path="./", fileName="data", data={}):
+    filePathNameWExt = './' + path + '/' + fileName + '.json'
+    with open(filePathNameWExt, 'w') as f:
+      json.dump(data, f)
 
-    @staticmethod
-    def readJSONFile(path="./", fileName="data"):
-        filePathNameWExt = './' + path + '/' + fileName + '.json'
-        with open(filePathNameWExt, 'r') as f:
-            return json.load(f)
+  @staticmethod
+  def readJSONFile(path="./", fileName="data"):
+    filePathNameWExt = './' + path + '/' + fileName + '.json'
+    with open(filePathNameWExt, 'r') as f:
+      return json.load(f)
 
-    def generateRemoveSet(self):
+  def generateRemoveSet(self):
+    self.generate()
+    self.removeCharacters()
+    self.leveler.setBoard(self.board)
+
+
+  def generateSeed(self, lvl=1, howMany=2):
+    data = Creator.readJSONFile()
+
+    for _ in range(howMany):
+      level = None
+            
+      while level != lvl:
         self.generate()
         self.removeCharacters()
         self.leveler.setBoard(self.board)
+        level = self.leveler.getLevel()
 
-
-    def generateSeed(self, lvl=1, howMany=2):
-        data = Creator.readJSONFile()
-
-        for _ in range(howMany):
-            level = None
-            
-            while level != lvl:
-                self.generate()
-                self.removeCharacters()
-                self.leveler.setBoard(self.board)
-                level = self.leveler.getLevel()
-
-            else:
-                data["level" + str(lvl)].append(copy.deepcopy(self.board))
+      else:
+        data["level" + str(lvl)].append(copy.deepcopy(self.board))
                 
-        Creator.writeToJSONFile(data=data)
-        print("Done!")
+      Creator.writeToJSONFile(data=data)
+      print("Done!")
  
     
 if __name__ == "__main__":
-    creator = Creator()
-    creator.setupJSONfile(1)
-    creator.generateSeed()
+  creator = Creator()
+  creator.setupJSONfile(1)
+  creator.generateSeed()
