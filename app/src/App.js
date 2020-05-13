da6d31c3f2c9a6ca4c1638b63440f88aae95562d
Nikol Skvarilova <skvarilovanikol@gmail.com>
2020-05-13 15:46:35 +0200

    Deleted redundant section

--- app/src/App.js
+++ app/src/App.js
@@ -4,6 +4,7 @@ import './App.scss'
 // my components
 import LandingPage from './Components/LandingPage/LandingPage.jsx'
 import Quote from './Components/Quote/Quote.jsx'
+import Card from './Components/Card/Card.jsx'
 
 function App() {
 
@@ -20,17 +21,20 @@ function App() {
 
   return (
     <div>
-      <section className="dark-container">
-        <LandingPage 
-          main_title={["It's not just", <br />, "sudoku."]}
-          sub_text="We have taken sudoku to a new level."
-        />
+      <LandingPage 
+        main_title={["It's not just", <br />, "sudoku."]}
+        sub_text="We have taken sudoku to a new level."
+      />
 
-        <Quote 
-          text="If you are curious, you'll find the puzzles around you. If you are determined, you will solve them."
-          author="Erno Rubik"
-        />
-      </section>
+      <Quote 
+        text="If you are curious, you'll find the puzzles around you. If you are determined, you will solve them."
+        author="Erno Rubik"
+      />
+
+      <Card
+        heading="About"
+        sub_text="Learn more about our team!"
+      />
     </div>
   )
 }
