4fd4ab1eff1cfe450a23ce404e25913be6aae21d
Nikol Skvarilova <skvarilovanikol@gmail.com>
2020-05-15 12:08:55 +0200

    Deleted Card, added TextSection and section container

--- app/src/App.js
+++ app/src/App.js
@@ -5,6 +5,7 @@ import './App.scss'
 import LandingPage from './Components/LandingPage/LandingPage.jsx'
 import Quote from './Components/Quote/Quote.jsx'
 import Card from './Components/Card/Card.jsx'
+import TextSection from './Components/TextSection/TextSection.jsx'
 
 function App() {
 
@@ -21,20 +22,24 @@ function App() {
 
   return (
     <div>
-      <LandingPage 
-        main_title={["It's not just", <br />, "sudoku."]}
-        sub_text="We have taken sudoku to a new level."
-      />
+      <section className="dark-container">
+        <LandingPage 
+          main_title={["It's not just", <br />, "sudoku."]}
+          sub_text="We have taken sudoku to a new level."
+        />
 
-      <Quote 
-        text="If you are curious, you'll find the puzzles around you. If you are determined, you will solve them."
-        author="Erno Rubik"
-      />
+        <Quote 
+          text="If you are curious, you'll find the puzzles around you. If you are determined, you will solve them."
+          author="Erno Rubik"
+        />
 
-      <Card
-        heading="About"
-        sub_text="Learn more about our team!"
-      />
+        <TextSection 
+          title="About project"
+          sub_title="A few word from the author."
+          text={[<p>Aliquam sollicitudin egestas pellentesque. Nulla porta sollicitudin lacus, ut finibus libero fermentum quis. Integer fermentum mattis ex, lacinia lobortis ex hendrerit in. Vivamus at ante imperdiet, lobortis felis vitae, efficitur massa.</p>, <p>Nam ex metus, venenatis id justo nec, tempus dictum mauris. Sed non nibh lorem. Phasellus et faucibus ligula, sit amet porttitor magna. Phasellus non volutpat dolor, nec lobortis ex. Ut sed nibh vestibulum, rhoncus urna sit amet, convallis orci. Aenean pharetra ante ac interdum tristique. Proin sit amet turpis lacus.</p>]}
+          img={require('./images/about_img.png')}
+        />
+      </section>
     </div>
   )
 }
