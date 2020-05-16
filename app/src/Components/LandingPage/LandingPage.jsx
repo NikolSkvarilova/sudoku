9397dc46ccb2b496cb12cb524bd3eaa59dd474ef
Nikol Skvarilova <skvarilovanikol@gmail.com>
2020-05-16 22:03:38 +0200

    Converted to functional component and added styles

--- app/src/Components/LandingPage/LandingPage.jsx
+++ app/src/Components/LandingPage/LandingPage.jsx
@@ -2,24 +2,26 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import './LandingPage.scss';
 
-export class LandingPage extends Component {
-  render() {
-    return (
-      <div className="landing_page_container">
-        <h1 className="main_title">{this.props.main_title}</h1>
-        
-        <div className="scroll-down_container">
-          <p className="sub_text">{this.props.sub_text}</p>
-          {/* Scroll Button */}
-        </div>
-      </div>
-    )
+const LandingPage = (props) => {
+  let styles = {
+    backgroundImage: `url(${props.bg_img})`
   }
+
+  return (
+    <div className="landing_page_container" style={styles}>
+      <h1 className="main_title">{props.main_title}</h1>
+      <div className="scroll-down_container">
+        <p className="sub_text">{props.sub_text}</p>
+        {/* Scroll Button */}
+      </div>
+    </div>
+  )
 }
 
 LandingPage.propTypes = {
   main_title: PropTypes.string.isRequired,
-  sub_text: PropTypes.string
+  sub_text: PropTypes.string,
+  bg_img: PropTypes.node
 }
 
 export default LandingPage;
