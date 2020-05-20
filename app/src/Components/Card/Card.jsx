5d395ec39ae999b2dc1030051d2b10cfbcbafee2
Nikol Skvarilova <skvarilovanikol@gmail.com>
2020-05-20 13:37:05 +0200

    Class component --> functional component

--- app/src/Components/Card/Card.jsx
+++ app/src/Components/Card/Card.jsx
@@ -2,20 +2,18 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types';
 import './Card.scss'
 
-export class Card extends Component {
-  render() {
-    return (
-      <div className="card-container">
-        <div className="card-img-section"></div>
+const Card = (props) => {
+  return (
+    <div className="card-container">
+      <div className="card-img-section"></div>
 
-        <div className="card-text-section">
-          <h2 className="card-heading">{this.props.heading}</h2>
-          <p className="card-text">{this.props.sub_text}</p>
-        </div>
-        <button className="card-btn">{this.props.btn_text ? this.props.btn_text : "Take me there!"}</button>
+      <div className="card-text-section">
+        <h2 className="card-heading">{props.heading}</h2>
+        <p className="card-text">{props.sub_text}</p>
       </div>
-    )
-  }
+      <button className="card-btn">{props.btn_text ? props.btn_text : "Take me there!"}</button>
+    </div>
+  )
 }
 
 Card.propTypes = {
