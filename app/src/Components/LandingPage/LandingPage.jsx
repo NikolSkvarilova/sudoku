c69129ec9528a677520bfe53df2d36c108692eba
Nikol Skvarilova <skvarilovanikol@gmail.com>
2020-05-20 16:19:37 +0200

    Missed calling the styles

--- app/src/Components/LandingPage/LandingPage.jsx
+++ app/src/Components/LandingPage/LandingPage.jsx
@@ -20,7 +20,7 @@ const LandingPage = (props) => {
     <div className="landing_page_container" style={bgStyles}>
       <h1 className="main_title" style={titleStyles}>{props.main_title}</h1>
       <div className="scroll-down_container">
-        <p className="sub_text">{props.sub_text}</p>
+        <p className="sub_text" style={subTextStyles}>{props.sub_text}</p>
         {/* Scroll Button */}
       </div>
     </div>
