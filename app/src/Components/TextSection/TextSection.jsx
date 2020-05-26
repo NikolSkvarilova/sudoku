3a6e7cbcdb8513f49e4f8af245a720778768ed57
Nikol Skvarilova <skvarilovanikol@gmail.com>
2020-05-26 20:15:41 +0200

    I'm gonna die

--- app/src/Components/TextSection/TextSection.jsx
+++ app/src/Components/TextSection/TextSection.jsx
@@ -3,6 +3,47 @@ import PropTypes from 'prop-types';
 import './TextSection.scss';
 
 const TextSection = (props) => {
+
+  const checkImgMobileAlignment = (img_align, img_align_mobile) => {
+    // If the image is aligned to the left and the user wants it 
+    // to be above the text on a mobile resolution, you must apply
+    // the "bot" class, because the image is before the text so when 
+    // 'flex-direction: column' is used, the img is automatically above the text.
+
+    // Same for the image on the left and on mobile resolution aligned 
+    // to the bottom, you must use 'flex-direction: column-reverse' - which is .top.
+
+    // If you switch the definitions in the .top and .bot class, you must 
+    // optimize it for the case where the picture is on the right from the text.
+
+    if (!img_align_mobile) {
+      return "top";
+    }
+
+    if (img_align === "left" && img_align_mobile === "top") {
+      return "bot";
+
+    } else if (img_align === "left" && img_align_mobile === "bot") {
+      return "top";
+    };
+
+    return img_align_mobile;
+  };
+
+
+  const checkImgPositionStyle = (img_align, img_position) => {
+    if (img_position == "to_edge" && img_align == "left") {
+      return {
+        'marginLeft': '0',
+        'width': '100%'
+      };
+    } else if (img_position == "to_edge" && img_align == "right") {
+      return {
+        'marginRight': '0',
+        'width': '100%'
+      };
+    }
+  };
   
   // Styles of the main title
   let titleStyles = {
@@ -18,42 +59,57 @@ const TextSection = (props) => {
   let textStyles = {
     'color': !! props.txt_color ? props.txt_color : 'black',
   };
-  
-  // Styles of the line next to apragraph
+   
+  // Styles of the line next to paragraph
   let lineStyles = {
     'background': !! props.line_color ? props.line_color : ""
   };
 
-  let imgAlignMobileClass = props.img_align_mobile ? props.img_align_mobile : "top";
+  // ---------------------------
 
-  if (props.img_align === "left" && props.img_align_mobile === "top") {
-    imgAlignMobileClass = "bot";
-  } else if (props.img_align === "left" && props.img_align_mobile === "bot") {
-    imgAlignMobileClass = "top";
-  }
+  // Styles for the main wrapper
+  let imgPositionStyle = checkImgPositionStyle(props.img_align, props.img_position);
+
+  // Class for aligning the image on the mobile screen
+  let imgAlignMobileClass = checkImgMobileAlignment(props.img_align, props.img_align_mobile);
 
   return (
-    <div className="text_section_container">
-      <div className={`text_section_wrapper ${imgAlignMobileClass}`}>
-        {/* Image on the left */}
-        {props.img_align == "left" ? <img className="img_half left" src={props.img} alt={props.alt}></img> : ""}
-
+    <div className={`text_section_container ${imgAlignMobileClass}`}>  
+      {/* Image on the left */}
+      {props.img_align == "left" ? 
+        <div className={`img_half_container ${!! props.img_mobile_whole_size ? "full_page_width" : ""}`}>
+          <img style={imgPositionStyle} className="img_half left" src={props.img} alt={props.alt}></img>
+        </div> 
+        : ""
+      }
+      
+      <div className="text_half_container">
         <div className="text_half_wrapper">
           {/* Rectangle thingy */}
           {!! props.line_color ? <div className="rectangle" style={lineStyles}></div> : ""}
 
           <div className="text_half">
+            {/* Main Title */}
             {!! props.title ? <h1 className="text_section_title" style={titleStyles}>{props.title}</h1> : ""}
+
+            {/* Subtitle */}
             {!! props.sub_title ? <h2 className="text_section_subtitle" style={subTitleStyles}>{props.sub_title}</h2> : ""}
+
+            {/* Text */}
             <article className="text" style={textStyles}>
               {props.text}
             </article>
           </div>
-        </div>
-
-        {/* Image on the right (also right is the default value) */}
-        {props.img_align != "left" ? <img className="img_half right" src={props.img} alt={props.alt}></img> : ""}
+        </div>        
       </div>
+      
+      {/* Image on the right (default value) */}
+      {props.img_align != "left" ? 
+        <div className={`img_half_container ${!! props.img_mobile_whole_size ? "full_page_width" : ""}`}>
+          <img style={imgPositionStyle} className="img_half right" src={props.img} alt={props.alt}></img>
+        </div> 
+        : ""
+      }
     </div>
   );
 }
