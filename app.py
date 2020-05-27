bc89ec7b397627b0e5b7aafd24a230d7342735d9
Nikol Skvarilova <skvarilovanikol@gmail.com>
2020-05-27 20:30:57 +0200

    Added generating map for generating sudoku from a seed

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 
 
 import json
+import random
 
 class App:
   def __init__(self):
@@ -10,6 +11,20 @@ class App:
   def readJSON():
     with open('data.json') as JSONdata:
       return json.load(JSONdata)
+
+  def generateMap(self, numOfTotems):
+    # numOfTotems represents the number of possible values 
+    # (in 9x9 sudoku, numOfTotems=9, because you have numbers from 1-9)
+    
+    posssibleNums = list(range(1, numOfTotems+1))
+    totemMap = {}
+
+    for i in range(numOfTotems):
+      # Pick a random number and remove it from the list
+      value = posssibleNums.pop(random.randrange(len(posssibleNums)))
+      totemMap[i+1] = value
+
+    return totemMap
 
 if __name__ == "__main__":
   app = App()
