abd5db1d1556f26aaf75006d3bd816ee6078aaef
Nikol Skvarilova <skvarilovanikol@gmail.com>
2020-06-07 15:33:51 +0200

    Added comments

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import random
 
 class App:
   def __init__(self):
+    # Dictionary containing all the seeds
     self.sudokuFile = App.readJSON()
 
   @staticmethod
@@ -13,7 +14,12 @@ class App:
       return json.load(JSONdata)
 
   def generateMap(self, numOfTotems):
-    # numOfTotems represents the number of possible values 
+    # Creates a dictionary for swapping numbers in the sudoku
+    
+    # Example: {1: 5, 2: 3, 3: 8} so all the ones in the sudoku become fives,
+    # all the twos become threes, all the threes becomes eights
+    
+    # param: numOfTotems = the number of possible values 
     # (in 9x9 sudoku, numOfTotems=9, because you have numbers from 1-9)
     
     posssibleNums = list(range(1, numOfTotems+1))
@@ -27,6 +33,12 @@ class App:
     return totemMap
 
   def mapSeed(self, sudoku, totemMap):
+    # Maps given sudoku: goes one number per round, looks it in the dictionary and changes it to 
+    # it's new value (original value is key, new value is value if key-value pairs dict)
+    
+    # param: sudoku = the sudoku you want to map
+    # param: totemMap = the dictionary telling what numbers should be changed to what
+
     for i in range(len(sudoku)):
       for j in range(len(sudoku[0])):
         if sudoku[i][j] in totemMap.keys():
@@ -34,30 +46,47 @@ class App:
 
     return sudoku
 
-  def reverseVertically(self, sudoku):
+  def flipVertically(self, sudoku):
+    # Takes a sudoku and flips it vertically 
+    # The numbers on the left goes to the right and vice versa
+
+    # param: sudoku = the sudoku you want to flip
+
     for row in sudoku:
       row.reverse()
 
     return sudoku
 
-  def reverseHorizontally(self, sudoku):
+  def flipHorizontally(self, sudoku):
+    # Takes a sudoku and flips it horizontally 
+    # The numbers on the top goes to the bottom and vice versa
+
+    # param: sudoku = the sudoku you want to flip
     for i in range(len(sudoku)):
+      # You need to get the numbers from the column
       col = []
       for j in range(len(sudoku)):
         col.append(sudoku[j][i])
 
+      # Flip the values in the col
       col.reverse()
 
+      # And put them back
       for q in range(len(sudoku)):
         sudoku[q][i] = col[q]
 
     return sudoku 
 
   def rotateSudoku(self, sudoku, lvl):
-    # lvl represents level of rotating the sudoku:
+    # Rotates sudoku clockwise
+
+    # param: sudoku = the sudoku you want to rotate
+    # param: lvl = level of rotating the sudoku (how many times)
+  
     # lvl 1 = 90°
     # lvl 2 = 180°
     # lvl 3 = 270°
+    # lvl 4 = 0° (stays as it is)
 
     for _ in range(lvl):
       # Zip return tuples - we want lists
@@ -66,20 +95,33 @@ class App:
     return sudoku
 
   def getSeed(self, category):
+    # Returns a random choosen sudoku from a specific category
+
+    # param: category = name of the category from the JSON file from which you want to select random sudoku
+
     return random.choice(self.sudokuFile[category])
 
   def generateFromSeed(self, category):
+    # Generates a sudoku from a seed
+
+    # param: category = from which category you want to get a sudoku
+    
+    # Get seed from the file
     sudoku = self.getSeed(category)
+    # Generate a dict for mapping sudoku (1 -> 4, 2 -> 9, 3 -> 7 etc.)
     totemMap = self.generateMap(len(sudoku))
+    # Map the sudoku
     sudoku = self.mapSeed(sudoku, totemMap)
 
+    # Do some other stuffs with the sudoku (50% chance for every transformation)
     if random.random() < 0.5:
-      sudoku = self.reverseHorizontally(sudoku)
+      sudoku = self.flipHorizontally(sudoku)
     
     if random.random() < 0.5:
-      sudoku = self.reverseVertically(sudoku)
+      sudoku = self.flipVertically(sudoku)
 
     if random.random() < 0.5:
+      # Here we also generate a random lvl of rotation
       sudoku = self.rotateSudoku(sudoku, random.randrange(0, 4))   
 
     return sudoku 
