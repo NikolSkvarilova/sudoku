6c4102bf57b15b49f8eecf61fb08a0719e54207b
Nikol Skvarilova <skvarilovanikol@gmail.com>
2020-06-16 16:57:00 +0200

    Added 'Play' page

---
+++ app/src/Components/Pages/Play/Play.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import './Play.scss';
+
+
+// my components
+// import LandingPage from '../../Sections/LandingPage/LandingPage.jsx';
+// import Quote from '../../Sections/Quote/Quote.jsx';
+// import Card from '../../Elements/Card/Card.jsx';
+// import TextSection from '../../Sections/TextSection/TextSection.jsx';
+// import ButtonSection from '../../Sections/ButtonSection/ButtonSection.jsx';
+// import Footer from '../../Sections/Footer/Footer.jsx';
+
+// Color palette
+// import colorPalette from '../../../color_palette.json'
+
+function Play() {
+
+  // Getting sudoku from backend
+  // fetch(`/play/get_sudoku/${props.lvl}.`)
+  fetch('/play/get_sudoku/5')
+    .then(response => response.json())
+    .then(data => console.log(data.sudoku))
+    .catch(err => console.log(err))
+
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default Play
