e916f726fbfae206c310bc42f297ca6e9d8eaa2b
Nikol Skvarilova <skvarilovanikol@gmail.com>
2020-06-24 16:26:39 +0200

    Fixed bug with wider webpage than screen

--- src/Components/Sections/Section/Section.jsx
+++ src/Components/Sections/Section/Section.jsx
@@ -3,7 +3,7 @@ import './Section.scss';
 
 const Section = (props) => {
   let sectionStyles = {
-    width:          !! props.width          ? props.width           : "100vw",
+    width:          !! props.width          ? props.width           : "100%",
     justifyContent: !! props.justifyContent ? props.justifyContent  : "center",   // horizontally
     background:     !! props.bg             ? props.bg              : "none",
     padding:        !! props.padding        ? props.padding         : "60px 0",
