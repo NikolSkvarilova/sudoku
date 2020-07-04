b42d124e50b90adf54efc290bdfc029223ebb282
Nikol Skvarilova <skvarilovanikol@gmail.com>
2020-07-04 15:50:09 +0200

    Implemented responsive navbar (basic)

--- src/App.js
+++ src/App.js
@@ -9,27 +9,52 @@ import Play from './Components/Pages/Play/Play.jsx'
 // Color palette
 import colorPalette from './color_palette.json'
 
-function App() {
-  return (
-    <Router>
-      <div className="App">
-        <nav style={ window.location.pathname === "/" ? { background: "none" } : { background: colorPalette.bgDark } }>
-          <div className="elements">
-            <a href="/#">Home</a>
-            <a href="/#about">About</a>
-            <a href="/#rules">Rules</a>
-            <a href="/#levels">Play</a>
-            <div id="indicator"></div>
-          </div>
-        </nav>
+class App extends React.Component {
+  constructor(props)  {
+    super(props);
 
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/play/:level" component={Play} />
-        </Switch>
-      </div>
-    </Router>
-  )
+    this.state = {
+      navActive: false
+    }
+  }
+  // let mainNav = document.getElementById("js-menu");
+  // let navBarToggle = document.getElementById("js-navbar-toggle");
+
+  // navBarToggle.addEventListener("click", function() {
+  //   mainNav.classList.toggle("active");
+  // });
+
+  toggleNavActive() {
+    this.setState({ navActive: !this.state.navActive })
+    console.log(this.state.navActive)
+  }
+
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <nav style={ window.location.pathname === "/" ? { background: "none" } : { background: colorPalette.bgDark } }>
+            <span class="navbar-toggle" onClick={ () => { this.toggleNavActive() }}>
+              <i class="fa fa-bars"></i>
+            </span>
+
+            <div className={ `elements ${ this.state.navActive ? 'active' : '' }` }>
+              <a href="/#">Home</a>
+              <a href="/#about">About</a>
+              <a href="/#rules">Rules</a>
+              <a href="/#levels">Play</a>
+              <div id="indicator"></div>
+            </div>
+          </nav>
+
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/play/:level" component={Play} />
+          </Switch>
+        </div>
+      </Router>
+    )
+  }
 }
 
 export default App
