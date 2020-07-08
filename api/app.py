166ef0e501143c3e3ba3ffcc92c95d994b997261
Nikol Skvarilova <skvarilovanikol@gmail.com>
2020-07-08 15:34:39 +0200

    Got the database working... kinda

--- api/app.py
+++ api/app.py
@@ -3,6 +3,7 @@ from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 
 from datetime import datetime
+import copy
 
 from getter import Getter
 from solver import Solver
@@ -12,7 +13,18 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db_sudoku.db'
 db = SQLAlchemy(app)
 
 getter = Getter()
-getter.generateDailySudoku()
+# getter.generateDailySudoku()
+getter.dailySudoku = [
+  [9, 1, 0, 3, 7, 8, 0, 0, 5], 
+  [6, 3, 8, 9, 5, 2, 1, 0, 4], 
+  [5, 2, 7, 6, 1, 4, 9, 3, 8], 
+  [3, 9, 6, 5, 2, 1, 8, 4, 7], 
+  [8, 0, 5, 4, 9, 6, 3, 1, 2], 
+  [1, 4, 2, 8, 3, 7, 5, 9, 6], 
+  [4, 5, 0, 7, 8, 3, 6, 2, 1], 
+  [7, 8, 1, 2, 6, 9, 4, 5, 3], 
+  [2, 0, 3, 0, 0, 5, 7, 8, 9]
+]
 
 # Database models
 class DailySudoku(db.Model):
@@ -35,13 +47,33 @@ def get_sudoku(lvl):
 @app.route('/play/check_sudoku', methods=['GET', 'POST'])
 def check_sudoku():
   if request.method == 'POST':
+    # Extract data from request
     sudoku = request.json[0]
     originalSudoku = request.json[1]
+    time = request.json[2]
+    name = request.json[3]
 
-    solver = Solver(originalSudoku)
+    date_time_obj = datetime.strptime(time, '%M:%S').date()
+
+    # Check if the sudoku is daily sudoku
+    dailySudoku = False
+
+    if originalSudoku == getter.getDailySudoku():
+      dailySudoku = True
+
+    # Solving the original sudoku
+    solver = Solver(copy.deepcopy(originalSudoku))
     solver.solve()
 
+    # Comparing sudokus
     if solver.board == sudoku:
+
+      # # If the sudoku is daily sudoku
+      if dailySudoku:
+        solver = DailySudokuSolver(sudokuID=getSudokuID(originalSudoku), name=name, time=date_time_obj)
+        db.session.add(solver)
+        db.session.commit()
+
       return{"solved_correctly": True}
 
     else:
@@ -50,8 +82,17 @@ def check_sudoku():
 
 @app.route('/play/getDailySudoku')
 def get_daily_sudoku():
+  # addDailySudokuToDatabase(getter.getDailySudoku())
   return {"sudoku": getter.getDailySudoku()}
 
+
+def getSudokuID(board):
+  return DailySudoku.query.filter_by(board=str(board)).first().id
+
+def addDailySudokuToDatabase(board):
+  sudoku = DailySudoku(board=str(board), date=datetime.utcnow())
+  db.session.add(sudoku)
+  db.session.commit()
 
 if __name__ == "__main__": 
   app.run(debug=True, port=5000)
