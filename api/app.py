cfe77bd34b70af4f309d978ca8bc44d7345e7712
Nikol Skvarilova <skvarilovanikol@gmail.com>
2020-08-24 18:27:22 +0200

    Wrong comment

--- api/app.py
+++ api/app.py
@@ -186,5 +186,5 @@ atexit.register(lambda: scheduler.shutdown())
 
 if __name__ == "__main__": 
   # Run app on port 5000 for local purposes
-  app.run(debug=True, port=5000)
-  # app.run(host='0.0.0.0', debug=False, port=os.environ.get('PORT', 80))
+  # app.run(debug=True, port=5000)
+  app.run(host='0.0.0.0', debug=False, port=os.environ.get('PORT', 80))
